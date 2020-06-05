6d23d98a3ecbe22522fb99fd3b39e345cff79fca
Sergey Leonov <s.leonov@fbs.com>
2020-06-05 10:31:42 +0300

    module3-task2: adding mockup variables for project

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var MINIMUM_Y_VALUE = 130;
+var MAXIMUM_Y_VALUE = 630;
+
+var IMAGES_PATHS = ['01', '02', '03', '04', '05', '06', '07', '08'];
+var FEATURES_LIST = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var CHECKIN_CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
+var PHOTOS_LIST = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var HOUSE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+
+var similarAdverts = [];
+
+function setupAdvert() {
+
+}
