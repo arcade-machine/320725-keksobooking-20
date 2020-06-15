1c914dbb1b6ad71158d9b6ce66e28687a50e1172
Sergey Leonov <s.leonov@fbs.com>
2020-06-15 17:38:14 +0300

    module5-task2: finishing with moduling project

--- js/form.js
+++ js/form.js
@@ -7,6 +7,16 @@
   var typeOfHouse = sendAdvertForm.querySelector('#type');
   var priceForNight = sendAdvertForm.querySelector('#price');
 
+  var cityMap = document.querySelector('.map');
+  var mapFiltersContainer = cityMap.querySelector('.map__filters-container');
+  var mapFiltersForm = mapFiltersContainer.querySelector('.map__filters');
+  var formFieldsets = sendAdvertForm.querySelectorAll('fieldset');
+  var filterSelects = mapFiltersForm.querySelectorAll('select');
+  var filtersFeatures = mapFiltersForm.querySelectorAll('fieldset');
+  var addressInput = sendAdvertForm.querySelector('#address');
+
+  addressInput.readOnly = true;
+
   checkinSelect.addEventListener('input', function () {
     checkoutSelect.value = checkinSelect.value;
   });
@@ -21,4 +31,38 @@
     priceForNight.min = minimumPrice;
     priceForNight.placeholder = minimumPrice;
   });
+
+  function disableOrActivateForm(shouldItBeDisabled) {
+    addressInput.disabled = shouldItBeDisabled;
+
+    formFieldsets.forEach(
+        function (fieldset) {
+          fieldset.disabled = shouldItBeDisabled;
+        }
+    );
+
+    filterSelects.forEach(
+        function (select) {
+          select.disabled = shouldItBeDisabled;
+        }
+    );
+
+    filtersFeatures.forEach(
+        function (feature) {
+          feature.disabled = shouldItBeDisabled;
+        }
+    );
+  }
+
+  disableOrActivateForm(true);
+
+  function activateForm() {
+    cityMap.classList.remove('map--faded');
+    sendAdvertForm.classList.remove('ad-form--disabled');
+    disableOrActivateForm(false);
+  }
+
+  window.formModule = {
+    activateForm: activateForm
+  };
 })();
--- js/main.js
+++ js/main.js
@@ -1,80 +1,28 @@
 'use strict';
 
-var cityMap = document.querySelector('.map');
-var mainPin = cityMap.querySelector('.map__pin--main');
-var mapFiltersContainer = cityMap.querySelector('.map__filters-container');
-var mapFiltersForm = mapFiltersContainer.querySelector('.map__filters');
+(function () {
+  var mainPin = document.querySelector('.map__pin--main');
 
-// --------------deactivate page-------------- //
-var sendAdvertForm = document.querySelector('.ad-form');
-var formFieldsets = sendAdvertForm.querySelectorAll('fieldset');
-var filterSelects = mapFiltersForm.querySelectorAll('select');
-var filtersFeatures = mapFiltersForm.querySelectorAll('fieldset');
+  function activatePage() {
+    window.formModule.activateForm();
+    window.mainPinModule.setUpAddressLocation(true);
 
-formFieldsets.forEach(
-    function (fieldset) {
-      fieldset.disabled = true;
-    }
-);
-
-filterSelects.forEach(
-    function (select) {
-      select.disabled = true;
-    }
-);
-
-filtersFeatures.forEach(
-    function (feature) {
-      feature.disabled = true;
-    }
-);
-
-var addressInput = sendAdvertForm.querySelector('#address');
-addressInput.disabled = true;
-
-// --------------activate page-------------- //
-function activateForm() {
-  cityMap.classList.remove('map--faded');
-  sendAdvertForm.classList.remove('ad-form--disabled');
-
-  formFieldsets.forEach(
-      function (fieldset) {
-        fieldset.disabled = false;
-      }
-  );
-
-  filterSelects.forEach(
-      function (select) {
-        select.disabled = false;
-      }
-  );
-
-  filtersFeatures.forEach(
-      function (feature) {
-        feature.disabled = false;
-      }
-  );
-}
-
-function activatePage() {
-  activateForm();
-  window.mainPinModule.setUpAddressLocation(true);
-
-  window.popup.renderSimilarAdverts(
-      window.pin.pinsDocumentFragment
-  );
-}
-
-mainPin.addEventListener('mousedown', function (evt) {
-  if (evt.button === 0) {
-    evt.preventDefault();
-    activatePage();
+    window.popup.renderSimilarAdverts(
+        window.pin.pinsDocumentFragment
+    );
   }
-});
 
-mainPin.addEventListener('keydown', function (evt) {
-  if (evt.key === 'Enter') {
-    evt.preventDefault();
-    activatePage();
-  }
-});
+  mainPin.addEventListener('mousedown', function (evt) {
+    if (evt.button === 0) {
+      evt.preventDefault();
+      activatePage();
+    }
+  });
+
+  mainPin.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+      evt.preventDefault();
+      activatePage();
+    }
+  });
+})();
