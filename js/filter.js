64c69dd1fdcf46998a89187ea9d0bd62d8cc6e6e
Sergey Leonov <s.leonov@fbs.com>
2020-07-03 16:03:42 +0300

    module7-task2: start for filter function

--- js/filter.js
+++ js/filter.js
@@ -1,16 +1,54 @@
 'use strict';
 
 (function () {
-  function filterForSelect(select, array, filteredProperty) {
-    var filteredArray = [];
+  var filterForm = document.querySelector('.map__filters');
 
-    filteredArray = array.slice();
+  filterForm.addEventListener('change', function () {
+    var formGuestInput = filterForm.elements.namedItem('housing-guests');
+    var formTypeInput = filterForm.elements.namedItem('housing-type');
+    var filteredArray = window.advert.similarAdverts.slice();
 
-    filteredArray.filter(
-        function (item) {
-          return item[filteredProperty];
-        }
-    );
+    if (formGuestInput.value !== 'any') {
+      filteredArray = filteredArray.filter(
+          function (item) {
+            return item.offer.guests === Number(formGuestInput.value);
+          }
+      );
+    } else {
+      filteredArray = window.advert.similarAdverts.slice();
+    }
+
+    if (formTypeInput.value !== 'any') {
+      filteredArray = filteredArray.filter(
+          function (item) {
+            return item.offer.type === formTypeInput.value;
+          }
+      );
+    } else {
+      filteredArray = window.advert.similarAdverts.slice();
+    }
+
+    console.log(filteredArray);
+
+    // formInputs.forEach(
+    //     function (input) {
+    //       console.log(input.value);
+    //     }
+    // );
+  });
+
+  function filterForSelect(input, array, filteredProperty) {
+    var filteredArray = array.slice;
+
+    if (input.value !== 'any') {
+      filteredArray = filteredArray.filter(
+          function (item) {
+            return item.offer[filteredProperty] === input.value;
+          }
+      );
+    }
+
+    return filteredArray;
   }
 
   window.filterModule = {
--- js/main.js
+++ js/main.js
@@ -10,16 +10,15 @@
     window.formModule.activateForm();
     window.mainPinModule.setUpAddressLocation(window.data.isPageActive);
 
-    console.log(window.pin.advertsToRender);
     window.pin.renderSimilarPins(
         window.pin.advertsToRender
     );
 
-    window.filterModule.filterForSelect(
-      '',
-      window.advert.similarAdverts,
-
-    );
+    // window.filterModule.filterForSelect(
+    //   '',
+    //   window.advert.similarAdverts,
+    //
+    // );
   }
 
   function deactivatePage() {
