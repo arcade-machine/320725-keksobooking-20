bcf703fc12ed30534e53331d5ff8a3fac0c74162
Sergey Leonov <s.leonov@fbs.com>
2020-07-08 12:03:03 +0300

    module8-task2: removing card.js module && adding deactivate status on click on reset button

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
   var mainPin = document.querySelector('.map__pin--main');
   var sendAdvertForm = document.querySelector('.ad-form');
 
+  var resetFormButton = sendAdvertForm.querySelector('.ad-form__reset');
+
   function activatePage() {
     window.data.isPageActive = true;
 
@@ -13,14 +15,19 @@
     window.pin.renderSimilarPins(
         window.pin.advertsToRender
     );
+
+    resetFormButton.addEventListener('click', deactivatePage);
   }
 
   function deactivatePage() {
     window.data.isPageActive = false;
 
     window.formModule.deactivateForm();
+    window.mainPinModule.setPinToDefaultStatus();
     window.mainPinModule.setUpAddressLocation(window.data.isPageActive);
     window.pin.removeSimilarPinsFromPage();
+
+    resetFormButton.removeEventListener('click', deactivatePage);
   }
 
   sendAdvertForm.addEventListener('submit', function (evt) {
--- js/mainPin.js
+++ js/mainPin.js
@@ -6,6 +6,56 @@
   var sendAdvertForm = document.querySelector('.ad-form');
   var addressInput = sendAdvertForm.querySelector('#address');
 
+  var defaultTopPosition = mainPin.offsetTop;
+  var defaultLeftPosition = mainPin.offsetLeft;
+
+  mainPin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startingCoordinates = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    function onMouseMove(mouseMove) {
+      mouseMove.preventDefault();
+
+      var movingCoordinates = {
+        x: startingCoordinates.x - mouseMove.clientX,
+        y: startingCoordinates.y - mouseMove.clientY
+      };
+
+      startingCoordinates = {
+        x: mouseMove.clientX,
+        y: mouseMove.clientY
+      };
+
+      var pinTopCoordinates = mainPin.offsetTop - movingCoordinates.y;
+      var pinLeftCoordinates = mainPin.offsetLeft - movingCoordinates.x;
+
+      if (pinTopCoordinates <= window.data.mapData.MINIMUM_Y_VALUE) {
+        pinTopCoordinates = window.data.mapData.MINIMUM_Y_VALUE;
+      } else if (pinTopCoordinates >= window.data.mapData.MAXIMUM_Y_VALUE) {
+        pinTopCoordinates = window.data.mapData.MAXIMUM_Y_VALUE;
+      }
+
+      mainPin.style.top = pinTopCoordinates + 'px';
+      mainPin.style.left = pinLeftCoordinates + 'px';
+
+      window.mainPinModule.setUpAddressLocation(true);
+    }
+
+    function onMouseUp(mouseUp) {
+      mouseUp.preventDefault();
+
+      mainPin.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    }
+
+    mainPin.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
   function setUpAddressLocation(isPageActive) {
     var leftPosition = mainPin.offsetLeft;
     var topPosition = mainPin.offsetTop;
@@ -18,9 +68,15 @@
     addressInput.value = addressX + ', ' + addressY;
   }
 
+  function setPinToDefaultStatus() {
+    mainPin.style.top = defaultTopPosition + 'px';
+    mainPin.style.left = defaultLeftPosition + 'px';
+  }
+
   setUpAddressLocation(false);
 
   window.mainPinModule = {
-    setUpAddressLocation: setUpAddressLocation
+    setUpAddressLocation: setUpAddressLocation,
+    setPinToDefaultStatus: setPinToDefaultStatus
   };
 })();
